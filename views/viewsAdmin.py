508fa459bd3f15465a0fab571827bd8cd2cf939e
Nestor Carvacho <98226435+NestorCarvacho@users.noreply.github.com>
2025-06-28 03:06:12 -0400

    Delete views directory

--- views/viewsAdmin.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-class AdminView(APIView):
-    def get(self, request):
-        return Response({'message': 'Admin endpoint'})
